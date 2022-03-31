fed554875b19853007da7ff9253eb2b014be7694
Sergey <forsubscriptions2019@gmail.com>
2022-03-31 09:25:01 +0300

    Updated configuration main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
   app.enableCors();
   const PORT = process.env.PORT || 4001;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   console.log(`App is started on ${PORT}`);
   await app.listen(PORT);
 }
