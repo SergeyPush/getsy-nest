12708caba343876693c135588a167ea14ca12487
Sergey <forsubscriptions2019@gmail.com>
2022-04-06 19:15:49 +0300

    Created product service and controller

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export enum ProductTypeEnum {
+  PRODUCT = 'product',
+  SERVICE = 'service',
+}
+export class CreateProductDto {
+  @IsNumber()
+  @IsOptional()
+  id?: number;
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+  @IsNotEmpty()
+  @IsString({ each: true })
+  features: string[];
+  @IsNotEmpty()
+  @IsEnum(ProductTypeEnum)
+  type: string;
+  @IsNumber()
+  @IsOptional()
+  price: number;
+}
